c85d83cd96b150d43d99eb3464604797b7566b54
our ame <you@example.com>
2022-10-20 02:22:10 +0300

    Изменения

--- Задание 3/Program.cs
+++ Задание 3/Program.cs
@@ -6,23 +6,24 @@
         for ( int j = 0; j < matr.GetLength( 1 ); ++j )
         {
             matr[ i, j ] = new Random().Next( minValue , maxValue );
+            
+            if ( i % 2 == 0  & j % 2 == 0  )
+                {
+                matr[ i , j ] = matr[ i , j ] * matr[ i , j ];
+                }
         }
     }
-    
+
     return matr;
 }
+
 
 void PrintTable( int[ , ] table )
 {
     for ( int i = 0; i < table.GetLength( 0 ); ++i )
     {
         for ( int j = 0; j < table.GetLength( 1 ); ++j )
-        {
-            if ( i % 2 == 0  & j % 2 == 0  )
-                {
-                table[ i , j ] = table[ i , j ] * table[ i , j ];
-                }
-                
+        {                
                 Console.Write( table[ i, j ] + " " );  
         }   
           Console.WriteLine();
@@ -35,3 +36,4 @@ int maxValue = Convert.ToInt32( Console.ReadLine() );
 
 int[ , ] table1 = GetMass( row, col, minValue, maxValue );
 PrintTable( table1 );
+
